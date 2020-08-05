8a1bbb9f79d8a60c5e3bc6a4807b562cd9b21329
freiremarcos <marcos.freire@zarpsystem.com.br>
2020-08-04 23:13:19 -0300

    refactor project

---
+++ src/Learning.CSharp.Nine/ExemploRelationalPatterns/LearningRelationalPatterns.cs
@@ -0,0 +1,31 @@
+﻿namespace Learning.CSharp.Nine.ExemploRelationalPatterns
+{
+    public class LearningRelationalPatterns
+    {
+        public enum ClassificacaoIMC
+        {
+            Magreza,
+            Normal,
+            Sobrepeso,
+            Obesidade,
+            ObesidadeGrave
+        }
+
+        public class MedidasPessoa
+        {
+            public double Peso { get; set; }
+            public double Altura { get; set; }
+            public double IMC => Peso / (Altura * Altura);
+        }
+
+        public static ClassificacaoIMC AnalisarPeso(double imc) =>
+                imc switch
+                {
+                    < 18.5 => ClassificacaoIMC.Magreza,
+                    < 25.0 => ClassificacaoIMC.Normal,
+                    < 29.9 => ClassificacaoIMC.Sobrepeso,
+                    < 40.0 => ClassificacaoIMC.Obesidade,
+                    _ => ClassificacaoIMC.ObesidadeGrave
+                };
+    }
+}
---
+++ src/Learning.CSharp.Nine/TargetTypedNew/LearningTargetTypedNew.cs
@@ -0,0 +1,27 @@
+﻿using Learning.CSharp.Nine.Common;
+
+namespace Learning.CSharp.Nine.TargetTypedNew
+{
+    public class LearningTargetTypedNewSample
+    {
+        public string Name { get; init; }
+        public string ReadOnlyProperty { get; init; }
+    }
+
+    public class LearningTargetTypedNew
+    {
+        public static void Start()
+        {
+            var oldWay = new LearningTargetTypedNewSample
+            {
+                Name = "oldWay",
+                ReadOnlyProperty = "oldWay"
+            };
+
+            LearningTargetTypedNewSample newWay = new() { ReadOnlyProperty = "new Way", Name = "new Way" };
+
+            PrinterHelper.Print(oldWay);
+            PrinterHelper.Print(newWay);
+        }
+    }
+}
